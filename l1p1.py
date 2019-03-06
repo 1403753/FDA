3d8f6d6267f24dafed35121eb55909cae41693fb
Thomas Torsney-Weir <torsneyt@gmail.com>
2019-03-06 18:54:35 +0100

    code for assignment

---
+++ l1p1.py
@@ -0,0 +1,26 @@
+
+##############
+# Question 1 #
+##############
+# Define sort function
+def sort_list(l):
+  pass
+
+##############
+# Question 2 #
+##############
+# Given a random number and a random digit, design an algorithm to place the 
+# digit so that the number is divisible by 3
+def div7(n, i):
+  pass 
+
+##############
+# Question 3 #
+##############
+# Extend the cubic function y = a_1 x^3 + a_2 x^2 + a_3 x + a_4 to handle a 
+# multi-variate input
+def cubic(x, A):
+  import numpy as np
+  pass
+
+
